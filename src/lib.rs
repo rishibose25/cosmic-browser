fbffb331d9c8784450d693a59537bd65dc6acd89
Rishi Bose <rishibose25@proton.me>
2026-03-16 14:47:43 +0530

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,6 @@
-// src/lib.rs
-
-// Main modules
-mod module1;
-mod module2;
-
-// Error types
-#[derive(Debug)]
-pub enum CosmicError {
-    NotFound,
-    BadRequest,
-    Unauthorized,
-    InternalServerError,
-}
-
-impl std::fmt::Display for CosmicError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}", self)
-    }
-}
-
-impl std::error::Error for CosmicError {}
+pub mod app;
+pub mod browser;
+pub mod ipc;
+pub mod sidebar;
+pub mod toolbar;
+pub mod webview_widget;
