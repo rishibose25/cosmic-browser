b946932c082d135bf548400fe393acb3716169e6
Rishi Bose <rishibose25@proton.me>
2026-03-16 17:23:11 +0530

    Update ipc.rs

--- src/ipc.rs
+++ src/ipc.rs
@@ -1,13 +1,14 @@
-use std::path::PathBuf;
 use tokio::sync::mpsc;
 
 #[derive(Debug, Clone)]
 pub enum WebViewEvent {
     TitleChanged        { tab_id: usize, title: String },
     UrlChanged          { tab_id: usize, url: String },
+    FaviconUrl          { tab_id: usize, url: String },
     LoadStarted         { tab_id: usize },
     LoadFinished        { tab_id: usize },
     LoadProgress        { tab_id: usize, progress: f64 },
+    CanGoChanged        { tab_id: usize, back: bool, forward: bool },
     IpcMessage          { tab_id: usize, body: String },
     NewWindowRequested  { tab_id: usize, url: String },
     DownloadStarted     { tab_id: usize, url: String, suggested_path: Option<String> },
